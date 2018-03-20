f77ad8b12a09c0ef04474057b942f84ac288cf3b
Cathleen <ca.schmalfuss@live.de>
2018-03-20 16:43:40 +0100

    initial commit with some Pong-prototype

---
+++ Assets/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        // Hit left racket?
+        if (col.gameObject.name == "Racket_left") {
+            // Calculate hit factor
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            // Calculate direction, make length = 1 via normalizing
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set velocity with dir speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit right racket?
+        if (col.gameObject.name == "Racket_right")
+        {
+            // Calculate hit factor
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            // Calculate direction, make length = 1 via normalizing
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set velocity with dir speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+
+    float hitFactor (Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+}
