ed4896480800fbc9765a11b5c5da4127ed883b25
Cathleen <ca.schmalfuss@live.de>
2018-03-24 01:38:18 +0100

    added pause and unfinished version of menu

---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseMenu : MonoBehaviour {
+	public Canvas menu;
+	public Text text1;
+	public Text text2;
+	public bool paused = false;
+
+	void Start () {
+		Debug.Log ("menu should be disabled");
+		menu.enabled = false;
+		text1.enabled = false;
+		text2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Cancel")) {
+			Debug.Log ("Escape pressed.");
+			paused = togglePause();
+		}
+
+		if (!paused) {
+			menu.enabled = false;
+			text1.enabled = false;
+			text2.enabled = false;
+		} else {
+			menu.enabled = true;
+			text1.enabled = true;
+			text2.enabled = true;
+		}
+	}
+	
+	void onGUI() {
+		if (paused) {
+			menu.enabled = false;
+			text1.enabled = false;
+			text2.enabled = false;
+		} else {
+			menu.enabled = true;
+			text1.enabled = true;
+			text2.enabled = true;
+		}
+	}
+
+	bool togglePause() {
+		Debug.Log ("Time scale:  " + Time.timeScale);
+		if (Time.timeScale == 0f) {
+			Time.timeScale = 1f;
+			return false;
+		} else {
+			Time.timeScale = 0f;
+			return true;
+		}
+	}
+}
